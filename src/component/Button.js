12ba7c93b0d789da076e8f29aff8df326aeb4818
Lamber Sobremesa <lamberlemos@gmail.com>
2022-05-06 23:06:07 +0200

    Initial commit

---
+++ src/component/Button.js
@@ -0,0 +1,15 @@
+import styledComponents from 'styled-components';
+import colors from '././utils/colors';
+
+
+const Button = styledComponents.div`
+    background-color: ${props} => props.
+`;
+
+export default () => {
+    return (
+        <Button>
+    
+        </Button>
+    );
+}
---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+const colors =  {
+    default: '#E0E0E0',
+    default_hover_focu: 'AEAEAE'
+};
+
+export default colors;
