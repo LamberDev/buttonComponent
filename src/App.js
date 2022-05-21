7d96bdaa4ab37d795166f95b407358c1948d9b78
LamberMsi <lamberlemos@gmail.com>
2022-05-21 20:30:59 +0200

    Adding final changes, using better css practices, good naming and displaying all button states properly

--- src/App.js
+++ src/App.js
@@ -3,55 +3,42 @@ import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button />
-        <hr></hr>
-        <Button variant="outline" />
-        <hr></hr>
-        <Button variant="text" />
-        <hr></hr>
-        <Button disableShadow />
-        <hr></hr>
-        <Button disabled />
-        <hr></hr>
-        <Button variant="text"  disabled />
-        <hr></hr>
-        <Button size="sm"/>
-        <hr></hr>
-        <Button variant="md"/>
-        <hr></hr>
-        <Button variant="lg"/>
-        <hr></hr>
-        <Button color="default"/>
-        <hr></hr>
-        <Button color="primary"/>
-        <hr></hr>
-        <Button color="secondary"/>
-        <hr></hr>
-        <Button color="danger"/>
-        <hr></hr>
-        <Button startIcon="delete_icon"/>
-        <hr></hr>
-        <Button endIcon="delete_icon"/>
-        <hr></hr>
-        <Button startIcon="add_icon"/>
-        <hr></hr>
-        <Button endIcon="add_icon"/>
-        <hr></hr>
-        <Button startIcon="star_icon"/>
-        <hr></hr>
-        <Button endIcon="star_icon"/>
-        <hr></hr>
-        <Button startIcon="thumb_up"/>
-        <hr></hr>
-        <Button endIcon="thumb_up"/>
-        <hr></hr>
-        <Button startIcon="thumb_down"/>
-        <hr></hr>
-        <Button endIcon="thumb_down"/>
-      </header>
-    </div>
+    <main className="App">
+        <div className='buttton_container'>
+          <Button />
+          <Button variant="outline" />
+          <Button variant="text" />
+          <Button disableShadow />
+          <Button disabled />
+          <Button variant="text"  disabled />
+        </div>
+
+        <div className='buttton_container'>
+          <Button size="sm"/>
+          <Button variant="md"/>
+          <Button variant="lg"/>
+        </div>
+
+        <div className='buttton_container'>
+          <Button color="default"/>
+          <Button color="primary"/>
+          <Button color="secondary" text="Secondary"/>
+          <Button color="danger" text="Danger"/>
+        </div>
+
+        <div className='buttton_container'>
+          <Button startIcon="delete_icon"/>
+          <Button endIcon="delete_icon"/>
+          <Button startIcon="add_icon"/>
+          <Button endIcon="add_icon"/>
+          <Button startIcon="star_icon"/>
+          <Button endIcon="star_icon"/>
+          <Button startIcon="thumb_up"/>
+          <Button endIcon="thumb_up"/>
+          <Button startIcon="thumb_down"/>
+          <Button endIcon="thumb_down"/>
+        </div>
+    </main>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import '../styles/styles.css';
+import '../styles/button_classes.css';
 import getButtonClasses from '../utils/getButtonClasses';
 import getIcon from '../utils/getIcons';
 
@@ -6,11 +6,12 @@ function Button(props){
 
     var buttonClasses = getButtonClasses(props);
     var icon = getIcon(props);
+    var text = props.text ? props.text : 'Default';
 
     return (
         <div id='button' className={buttonClasses}>
             <img alt="icon" style={{display: props.startIcon ? 'inline-block' : 'none'}} src={icon} className='icon' ></img>
-            <span>Default</span>
+            <span>{text}</span>
             <img alt="icon" style={{display: props.endIcon ? 'inline-block' : 'none'}} src={icon} className='icon'></img>
         </div> 
     )
